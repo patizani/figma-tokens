c6e7dbd2d0c805951e246efb7768f17a8d601f0a
patizani <pzanirati@gmail.com>
2023-03-17 11:30:15 -0300

    primeiro commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 17 Mar 2023 14:16:10 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50506c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8b5cf6"
+};
